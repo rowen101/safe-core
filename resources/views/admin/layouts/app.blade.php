e68f5521d08bb88cb4ddf27592bb03bc39e6e1ee
Rowen <rgrowengonzales66@gmail.com>
2024-02-06 10:00:47 +0800

    add crf_token vue js login

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,10 +4,11 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SLIVSC</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="icon" type="image/png" sizes="16x16" href="http://safexpress.com.ph/favicon.ico">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
      <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -39,7 +40,7 @@
     </script> --}}
 
 
-    
+
 </body>
 
 </html>
