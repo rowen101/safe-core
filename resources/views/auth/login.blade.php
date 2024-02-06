c6b19b98adcf3db6a0c45cf80bf5a7c6e89f32d1
Rowen <rgrowengonzales66@gmail.com>
2024-02-06 10:51:44 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Safexpress Logistics Inc.<title>
+    <title>Safexpress<title>
         <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
