87e3d7a608e55db13355a60212524531a85beca5
Rowen <rgrowengonzales66@gmail.com>
2024-02-24 10:00:45 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    define: {
+        // enable hydration mismatch details in production build
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+      },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-               
+
             ],
             refresh: true,
         }),
